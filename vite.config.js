357ae289e297174e447265e41eb1d154060ce462
A-AlQershi <ahmedalqershi66@gmail.com>
2026-01-14 14:57:19 +0100

    Change vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/your-new-repo-name/",
+
   plugins: [react()],
   test: {
-    environment: 'jsdom',
-    setupFiles: './vitest.setup.js',
+    environment: "jsdom",
+    setupFiles: "./vitest.setup.js",
     globals: true,
     css: true,
-  }
-})
+  },
+});
