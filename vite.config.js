8b30a0522a170586d1520fd5805855c8d1d09928
A-AlQershi <ahmedalqershi66@gmail.com>
2026-01-14 14:59:46 +0100

    Change vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/your-new-repo-name/",
+  base: "/Aden_Grand_Library/",
 
   plugins: [react()],
   test: {
